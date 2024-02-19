dc334790a45f8e481b808c109db0d87e65ff5d4b
Saviour Armstrong <saviourarmstrong100@gmail.com>
2024-02-19 07:12:40 -0800

    corrected EmailService

--- PRMS.Core/Abstractions/IEmailService.cs
+++ PRMS.Core/Abstractions/IEmailService.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace PRMS.Core.Abstractions;
 
-namespace PRMS.Core.Abstractions
-{
     public interface IEmailService
     {
         Task<string> SendEmailAsync(string recipientEmail, string subject, string body);
     }
-}
+

acb78155669f71c0be29790cda68b9c92c5ca646
Saviour Armstrong <saviourarmstrong100@gmail.com>
2024-02-19 06:23:21 -0800

    Emailervices Completed

--- PRMS.Api/Program.cs
+++ PRMS.Api/Program.cs
@@ -1,6 +1,8 @@
 using PRMS.Api.Extensions;
 using PRMS.Api.Middlewares;
+using PRMS.Core.Abstractions;
 using PRMS.Data.Seed;
+using RYT.Services.Emailing;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbServices(builder.Configuration);
 builder.Services.AddServices(builder.Configuration);
+builder.Services.AddScoped<IEmailService, EmailService>();
 
 var app = builder.Build();
 
---
+++ PRMS.Core/Abstractions/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRMS.Core.Abstractions
+{
+    public interface IEmailService
+    {
+        Task<string> SendEmailAsync(string recipientEmail, string subject, string body);
+    }
+}

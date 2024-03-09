4a493227834edc6d5d0fa9cdbf129f1c08a2876d
Saviour Armstrong <saviourarmstrong100@gmail.com>
2024-03-09 12:57:01 -0800

    changed controller to Api controller

--- PRMS.Api/Controllers/PhysicianController.cs
+++ PRMS.Api/Controllers/PhysicianController.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PRMS.Api.Controllers
 {
-    public class PhysicianController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhysicianController : ControllerBase
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
     }
 }

2ea77dc5cc647613315ab1bc69353217f835c0f5
Marvellous Odemwingie <46074808+marvode@users.noreply.github.com>
2024-03-09 22:37:50 +0100

    Update PhysicianController.cs

--- PRMS.Api/Controllers/PhysicianController.cs
+++ PRMS.Api/Controllers/PhysicianController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace PRMS.Api.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("api/v1/physicians")]    
 public class PhysicianController : ControllerBase
 {
 }
-

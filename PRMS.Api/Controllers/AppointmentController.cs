06ed6b9fae0892c931ef22efadb22c5f376588a5
Kayode Abereowo <kayodeabereowo@gmail.com>
2024-03-12 12:36:17 +0100

    Implemented fetch Physician's appointments (User)

---
+++ PRMS.Api/Controllers/AppointmentController.cs
@@ -0,0 +1,33 @@
+﻿using PRMS.Core.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using PRMS.Core.Dtos;
+using Microsoft.AspNetCore.Identity;
+using PRMS.Domain.Entities;
+using PRMS.Api.Dtos;
+
+namespace PRMS.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/appointments")]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IAppointmentService _appointmentService;
+        private readonly UserManager<User> _userManager;
+
+        public AppointmentController(IAppointmentService appointmentService, UserManager<User> userManager)
+        {
+            _appointmentService = appointmentService;
+            _userManager = userManager;
+        }
+
+        [HttpGet("{physicianId}")]
+        public async Task<IActionResult> GetPhysicianAppointments([FromRoute] string physicianId, [FromQuery] DateTimeOffset startDate, [FromQuery] DateTimeOffset endDate)
+        {
+            var userId = _userManager.GetUserId(User);
+            var result = _appointmentService.GetAppointmentsForPhysician(physicianId, startDate, endDate);
+            if (result.Result.IsFailure)
+                return BadRequest(ResponseDto<object>.Failure(result.Result.Errors));
+            return Ok(ResponseDto<object>.Success());
+        }
+    }
+}
---
+++ PRMS.Core/Services/AppointmentService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PRMS.Core.Abstractions;
+using PRMS.Core.Dtos;
+using PRMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+namespace PRMS.Core.Services
+{
+    public class AppointmentService : IAppointmentService
+    {
+        private readonly IRepository _repository;
+        private readonly UserManager<User> _userManager;
+
+        public AppointmentService(IRepository repository, UserManager<User> userManager)
+        {
+            _repository = repository;
+            _userManager = userManager;
+        }
+
+        public async Task<Result> GetAppointmentsForPhysician(string physicianId, DateTimeOffset startDate, DateTimeOffset endDate)
+        {
+            var physician = await _userManager.FindByIdAsync(physicianId);
+            if (physician == null)
+                return new Error[] { new("Auth.Error", "user not found") };
+            var physicianAppointments = _repository.GetAll<Appointment>()
+                .Where(a => a.PhysicianId == physician.Id && a.Date >= startDate && a.Date <= endDate)
+                .Select(c => new FetchPhysicianAppointmentsUserDto(c.Date)).ToList();
+            return Result.Success();
+        }
+    }
+}

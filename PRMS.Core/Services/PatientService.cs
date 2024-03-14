bb11c0fb1789d0fae0887edfc7c59e7236462ebf
Marvellous Odemwingie <odemwingiemarv@gmail.com>
2024-03-14 13:53:13 +0100

    update patient creation logic

--- PRMS.Core/Services/PatientService.cs
+++ PRMS.Core/Services/PatientService.cs
@@ -10,105 +10,105 @@ namespace PRMS.Core.Services;
 
 public class PatientService : IPatientService
 {
-	private readonly IRepository _repository;
-	private readonly UserManager<User> _userManager;
-	private readonly IUnitOfWork _unitOfWork;
+    private readonly IRepository _repository;
+    private readonly UserManager<User> _userManager;
+    private readonly IUnitOfWork _unitOfWork;
 
-	public PatientService(IRepository repository, UserManager<User> userManager, IUnitOfWork unitOfWork)
-	{
-		_repository = repository;
-		_userManager = userManager;
-		_unitOfWork = unitOfWork;
-	}
+    public PatientService(IRepository repository, UserManager<User> userManager, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _userManager = userManager;
+        _unitOfWork = unitOfWork;
+    }
 
-	public async Task<Result> UpdateFromPatientAsync(UpdatePatientFromPatientDto dto, string userId)
-	{
-		var user = await _userManager.FindByIdAsync(userId);
+    public async Task<Result> UpdateFromPatientAsync(UpdatePatientFromPatientDto dto, string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
 
-		if (user == null)
-		{
-			return new Error[] { new("User.Error", "User Not Found") };
-		}
+        if (user == null)
+        {
+            return new Error[] { new("User.Error", "User Not Found") };
+        }
 
-		var patient = _repository.GetAll<Patient>()
-			.FirstOrDefault(x => x.UserId == user.Id);
+        var patient = _repository.GetAll<Patient>()
+            .FirstOrDefault(x => x.UserId == user.Id);
 
-		if (patient == null)
-		{
-			return new Error[] { new("Patients.Error", "Patient Not Found") };
-		}
+        if (patient == null)
+        {
+            return new Error[] { new("Patients.Error", "Patient Not Found") };
+        }
 
-		patient.DateOfBirth = dto.DateOfBirth ?? patient.DateOfBirth;
-		patient.Gender = dto.Gender ?? patient.Gender;
-		patient.BloodGroup = dto.BloodGroup ?? patient.BloodGroup;
-		patient.PrimaryPhysicanName = dto.PrimaryPhysicanName ?? patient.PrimaryPhysicanName;
-		patient.PrimaryPhysicanEmail = dto.PrimaryPhysicanEmail ?? patient.PrimaryPhysicanEmail;
-		patient.PrimaryPhysicanPhoneNo = dto.PrimaryPhysicanPhoneNo ?? patient.PrimaryPhysicanPhoneNo;
-		patient.Height = dto.Height ?? patient.Height;
-		patient.Weight = dto.Weight ?? patient.Weight;
-		patient.EmergencyContactName = dto.EmergencyContactName ?? patient.EmergencyContactName;
-		patient.EmergencyContactPhoneNo = dto.EmergencyContactPhoneNo ?? patient.EmergencyContactPhoneNo;
-		patient.EmergencyContactRelationship = dto.EmergencyContactRelationship ?? patient.EmergencyContactRelationship;
+        patient.DateOfBirth = dto.DateOfBirth ?? patient.DateOfBirth;
+        patient.Gender = dto.Gender ?? patient.Gender;
+        patient.BloodGroup = dto.BloodGroup ?? patient.BloodGroup;
+        patient.PrimaryPhysicanName = dto.PrimaryPhysicanName ?? patient.PrimaryPhysicanName;
+        patient.PrimaryPhysicanEmail = dto.PrimaryPhysicanEmail ?? patient.PrimaryPhysicanEmail;
+        patient.PrimaryPhysicanPhoneNo = dto.PrimaryPhysicanPhoneNo ?? patient.PrimaryPhysicanPhoneNo;
+        patient.Height = dto.Height ?? patient.Height;
+        patient.Weight = dto.Weight ?? patient.Weight;
+        patient.EmergencyContactName = dto.EmergencyContactName ?? patient.EmergencyContactName;
+        patient.EmergencyContactPhoneNo = dto.EmergencyContactPhoneNo ?? patient.EmergencyContactPhoneNo;
+        patient.EmergencyContactRelationship = dto.EmergencyContactRelationship ?? patient.EmergencyContactRelationship;
 
-		user.FirstName = dto.FirstName ?? user.FirstName;
-		user.MiddleName = dto.MiddleName ?? user.MiddleName;
-		user.LastName = dto.LastName ?? user.LastName;
-		user.PhoneNumber = dto.PhoneNumber ?? user.PhoneNumber;
+        user.FirstName = dto.FirstName ?? user.FirstName;
+        user.MiddleName = dto.MiddleName ?? user.MiddleName;
+        user.LastName = dto.LastName ?? user.LastName;
+        user.PhoneNumber = dto.PhoneNumber ?? user.PhoneNumber;
 
-		_repository.Update(patient);
-		await _userManager.UpdateAsync(user);
-		await _unitOfWork.SaveChangesAsync();
+        _repository.Update(patient);
+        await _userManager.UpdateAsync(user);
+        await _unitOfWork.SaveChangesAsync();
 
-		return Result.Success();
-	}
-  
-	public async Task<Result> GetPatientAppointments(string userId, string? status, PaginationFilter paginationFilter)
-	{
-		var user = await _userManager.FindByIdAsync(userId);
-		if (user == null)
-			return new Error[] { new("User.Error", "User Not Found") };
+        return Result.Success();
+    }
 
-		var patient = _repository.GetAll<Patient>().FirstOrDefault(x => x.UserId == user.Id);
-		if (patient == null)
-			return new Error[] { new("User.Error", "Patient Not Found") };
+    public async Task<Result> GetPatientAppointments(string userId, string? status, PaginationFilter paginationFilter)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
+            return new Error[] { new("User.Error", "User Not Found") };
 
-		IQueryable<Appointment> patientAppointments = _repository.GetAll<Appointment>()
-		.Where(a => a.PatientId == patient.Id)
-		.OrderByDescending(c => c.Date)
-		.Include(p => p.Physician)
-		.ThenInclude(u => u.User);
+        var patient = _repository.GetAll<Patient>().FirstOrDefault(x => x.UserId == user.Id);
+        if (patient == null)
+            return new Error[] { new("User.Error", "Patient Not Found") };
 
-		if (!string.IsNullOrEmpty(status))
-		{
-			switch (status.ToLower())
-			{
-				case "upcoming":
-					patientAppointments = patientAppointments.Where(a => a.Status == AppointmentStatus.Pending);
-					break;
-				case "completed":
-					patientAppointments = patientAppointments.Where(a => a.Status == AppointmentStatus.Completed);
-					break;
-				case "cancelled":
-					patientAppointments = patientAppointments.Where(a => a.Status == AppointmentStatus.Cancelled);
-					break;
-				default:
+        IQueryable<Appointment> patientAppointments = _repository.GetAll<Appointment>()
+            .Where(a => a.PatientId == patient.Id)
+            .OrderByDescending(c => c.Date)
+            .Include(p => p.Physician)
+            .ThenInclude(u => u.User);
 
-					return new Error[] { new("Status.Error", "Invalid status entered") };
-			}
-		}
+        if (!string.IsNullOrEmpty(status))
+        {
+            switch (status.ToLower())
+            {
+                case "upcoming":
+                    patientAppointments = patientAppointments.Where(a => a.Status == AppointmentStatus.Pending);
+                    break;
+                case "completed":
+                    patientAppointments = patientAppointments.Where(a => a.Status == AppointmentStatus.Completed);
+                    break;
+                case "cancelled":
+                    patientAppointments = patientAppointments.Where(a => a.Status == AppointmentStatus.Cancelled);
+                    break;
+                default:
 
-		var appointmentsToReturn = patientAppointments
-			.Select(p => new PatientAppointmentsToReturnDTO(
-				$"{p.Physician.User.FirstName} {p.Physician.User.LastName}",
-				p.Physician.Speciality,
-				p.Physician.User.ImageUrl,
-				p.Physician.MedicalCenter.Name,
-				$"{p.Physician.MedicalCenter.Address.Street} {p.Physician.MedicalCenter.Address.City} {p.Physician.MedicalCenter.Address.State}",
-				p.Date))
-			.Paginate(paginationFilter);
+                    return new Error[] { new("Status.Error", "Invalid status entered") };
+            }
+        }
 
-		return Result.Success(appointmentsToReturn);
-	}
+        var appointmentsToReturn = patientAppointments
+            .Select(p => new PatientAppointmentsToReturnDTO(
+                $"{p.Physician.User.FirstName} {p.Physician.User.LastName}",
+                p.Physician.Speciality,
+                p.Physician.User.ImageUrl,
+                p.Physician.MedicalCenter.Name,
+                $"{p.Physician.MedicalCenter.Address.Street} {p.Physician.MedicalCenter.Address.City} {p.Physician.MedicalCenter.Address.State}",
+                p.Date))
+            .Paginate(paginationFilter);
+
+        return Result.Success(appointmentsToReturn);
+    }
 
     public async Task<Result> CreatePatient(string userId, CreatePatientFromUserDto patientDto)
     {
@@ -123,10 +123,24 @@ public class PatientService : IPatientService
         existingUser.LastName = patientDto.LastName ?? existingUser.LastName;
         existingUser.MiddleName = patientDto.MiddleName ?? existingUser.MiddleName;
         existingUser.PhoneNumber = patientDto.PhoneNumber ?? existingUser.PhoneNumber;
-        existingUser.Address.Street = patientDto.Address.Street ?? existingUser.Address.Street;
-        existingUser.Address.City = patientDto.Address.City ?? existingUser.Address.City;
-        existingUser.Address.State = patientDto.Address.State ?? existingUser.Address.State;
-        existingUser.Address.Country = patientDto.Address.Country ?? existingUser.Address.Country;
+
+        if (existingUser.Address == null && patientDto.Address != null)
+        {
+            existingUser.Address = new Address
+            {
+                Street = patientDto.Address.Street,
+                City = patientDto.Address.City,
+                State = patientDto.Address.State,
+                Country = patientDto.Address.Country
+            };
+        }
+        else
+        {
+            existingUser.Address.Street = patientDto.Address.Street;
+            existingUser.Address.City = patientDto.Address.City;
+            existingUser.Address.State = patientDto.Address.State;
+            existingUser.Address.Country = patientDto.Address.Country;
+        }
 
         await _userManager.UpdateAsync(existingUser);
 

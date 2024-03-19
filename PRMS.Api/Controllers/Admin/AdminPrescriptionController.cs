f37956a069138a00b83e54fa3080ece583cc5ca8
Marvellous Odemwingie <odemwingiemarv@gmail.com>
2024-03-19 22:00:43 +0100

    Enable authorization for AdminPrescriptionController

--- PRMS.Api/Controllers/Admin/AdminPrescriptionController.cs
+++ PRMS.Api/Controllers/Admin/AdminPrescriptionController.cs
@@ -11,7 +11,7 @@ using PRMS.Domain.Entities;
 namespace PRMS.Api.Controllers.Admin;
 
 [ApiController]
-// [Authorize(Roles = RolesConstant.Admin)]
+[Authorize(Roles = RolesConstant.Admin)]
 [Route("api/v1/admin/prescription/{patientUserId}")]
 public class AdminPrescriptionController : ControllerBase
 {
@@ -29,8 +29,8 @@ public class AdminPrescriptionController : ControllerBase
     {
         if (!ModelState.IsValid)
             return BadRequest(ResponseDto<object>.Failure(ModelState.GetErrors()));
-        // var physicianUserId = _userManager.GetUserId(User);
-        var physicianUserId = "59fd28b1-4f82-4f9e-bdc0-80661d850e42";
+        
+        var physicianUserId = _userManager.GetUserId(User);
         var result = await _prescriptionService.CreatePrescription(patientUserId, physicianUserId!, prescriptionDto);
         if (result.IsFailure)
             return BadRequest(ResponseDto<object>.Failure(result.Errors));
